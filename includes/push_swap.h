3eb8fead8adf3ae1e5c84a18fa87f08742a6eec6
fallard21 <bassirov@yandex.ru>
2020-06-19 22:45:08 +0300

    fix tabs

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tima <tima@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/07 05:57:00 by tima              #+#    #+#             */
-/*   Updated: 2020/06/17 16:19:25 by tima             ###   ########.fr       */
+/*   Updated: 2020/06/19 22:44:34 by tima             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,51 +74,49 @@ int					min(int a, int b);
 /*
 ** >---------------------------< OPERATIONS >--------------------------<
 */
-void    do_sa_sb(t_stack *stack_0, int size);
-void    do_pa_pb(t_ps *push, t_stack *stack_1, t_stack *stack_2, int size);
-void    do_pa(t_ps *push, t_stack *stack_A, t_stack *stack_B);
-void    do_pb(t_ps *push, t_stack *stack_B, t_stack *stack_A);
-void    do_ra_rb(t_stack *stack_0, int size);
-void    do_rra_rrb(t_stack *stack_0, int size);
-void	do_operations(const char *line, t_ps *push);
+void				do_sa_sb(t_stack *stack_0, int size);
+void				do_pa_pb(t_ps *push, t_stack *stack_1, t_stack *stack_2, int size);
+void				do_pa(t_ps *push, t_stack *stack_A, t_stack *stack_B);
+void				do_pb(t_ps *push, t_stack *stack_B, t_stack *stack_A);
+void				do_ra_rb(t_stack *stack_0, int size);
+void				do_rra_rrb(t_stack *stack_0, int size);
+void				do_operations(const char *line, t_ps *push);
 
 /*
 ** >--------------------------< PRE-SORTING >--------------------------<
 */
-void	presorting(t_ps *push);
-void	search_limits(t_ps *push);
-void	push_all_to_b(t_ps *push);
-void	presorting_diff(t_ps *push, int diff);
-int		mid_value(t_ps *push, t_stack *stack_A);
+void				presorting(t_ps *push);
+void				search_limits(t_ps *push);
+void				push_all_to_b(t_ps *push);
+void				presorting_diff(t_ps *push, int diff);
+int					mid_value(t_ps *push, t_stack *stack_A);
 
 /*
 ** >----------------------------< ALGORIHM >---------------------------<
 */
-void 	start_sorting(t_ps *push);
-void	choosing_algorithm(t_ps *push, int num);
-void	algo_3_elem(t_ps *push);
-void	algo_low_stack(t_ps *push);
-
-void 	algorithm_zero(t_ps *push, t_stack *tmp);
-void	algorithm_cost1(t_ps *push, t_stack *tmp);
-void	algorithm_main(t_ps *push, t_stack *tmp, int rot, int rev_rot);
-
-void	normalize_stack(t_ps *push, t_stack *stack_A);
+void				start_sorting(t_ps *push);
+void				choosing_algorithm(t_ps *push, int num);
+void				algo_3_elem(t_ps *push);
+void				algo_low_stack(t_ps *push);
+void				algorithm_zero(t_ps *push, t_stack *tmp);
+void				algorithm_cost1(t_ps *push, t_stack *tmp);
+void				algorithm_main(t_ps *push, t_stack *tmp, int rot, int rev_rot);
+void				normalize_stack(t_ps *push, t_stack *stack_A);
 
 /*
 ** >------------------------< LEAKS TREATMENT >------------------------<
 */
-void    free_split(char ***str);
-void	free_list(t_stack **head);
-int		error_free(t_ps *push);
-int		correct_free(t_ps *push);
+void				free_split(char ***str);
+void				free_list(t_stack **head);
+int					error_free(t_ps *push);
+int					correct_free(t_ps *push);
 
 /*
 ** >----------------------------< CHECKER >----------------------------<
 */
-void	do_operations_1(const char *line, t_ps *push);
-int		read_operations(t_ps *push);
-int		validate_options(const char *str);
+void				do_operations_1(const char *line, t_ps *push);
+int					read_operations(t_ps *push);
+int					validate_options(const char *str);
 
 //void	print_args(t_stack *stack_A, t_stack *stack_B); // DEL
 
